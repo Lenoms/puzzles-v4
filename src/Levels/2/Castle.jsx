a332c2f00ee853b6aab34f5a8a8e600180b22a57
Daniel <danielwatson230@gmail.com>
2023-12-13 12:56:31 +1100

    Castle: Add images of cart and path. Position appropriately

--- src/Levels/2/Castle.jsx
+++ src/Levels/2/Castle.jsx
@@ -1,16 +1,26 @@
 import AnswerAndHintBox from "../../components/AnswerAndHintBox";
 import "./Castle.css";
 import castle from './CastleImage.png'
+import path from './PathImage.png'
+import cabbageCart from './CabbageCartImage.png'
 
 function Castle() {
   return (
     <div className="level-castle-div">
       <AnswerAndHintBox />
       <div className="castle-container">
-      <img src={castle}></img>
+        <img src={castle}></img>
+      </div>
+      <div className="path-container">
+        <img style={{width: "100%"}} src={path}></img>
+      </div>
+      <div className="cabbage-container">
+        <img src={cabbageCart}></img>
       </div>
     </div>
   );
 }
+
+// Put the colour of the letters in order on a flag or something
 
 export default Castle;
