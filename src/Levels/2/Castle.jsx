663cc7a5eadf250f9fe6279dd0e635e0725b77a5
Daniel <danielwatson230@gmail.com>
2023-12-23 21:07:11 +1300

    Castle: Added houses, sky, and road

--- src/Levels/2/Castle.jsx
+++ src/Levels/2/Castle.jsx
@@ -3,15 +3,23 @@ import "./Castle.css";
 import castle from './CastleImage.png'
 import path from './PathImage.png'
 import cabbageCart from './CabbageCartImage.png'
+import house from './HouseImage.png'
 
 function Castle() {
   return (
     <div className="level-castle-div">
       <AnswerAndHintBox />
-      <div className="images-container">
+      <div className="sky"></div>
+      <div className="castle-images-container">
         <img className="castle-image" src={castle}></img>
         <img className="path-image" style={{width: "100%"}} src={path}></img>
         <img className="cabbage-cart-image" src={cabbageCart}></img>
+      </div>
+      <div className="village-images-container">
+        <div className="village-road"></div>
+      <img className="house-image" src={house}></img>
+      <img className="house-image" src={house}></img>
+      <img className="house-image" src={house}></img>
       </div>
 
     </div>
